d268f7be3030544565d5d0781e2cbefe7009f4d1
Prince Bodarya <princebodarya23@gmail.com>
2026-02-04 11:31:55 +0530

    date:04/02/2026 work

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student, Product, marks
+
+
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(marks)
+admin.site.register(Product)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+   
+
+    class Meta:
+        db_table = 'student'
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'product'
+
+class marks(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    score = models.IntegerField()
+    email=models.EmailField(null=True)
+
+    class Meta:
+        db_table = 'marks'
